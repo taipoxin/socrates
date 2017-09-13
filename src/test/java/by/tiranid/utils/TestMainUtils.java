f1474dafb4aadc135243d958fee6193db7625eae
tiranid99 <ermakovichdmitriy@gmail.com>
2017-09-13 15:53:45 +0300

    added MainUtils testing + junit dep-cy

---
+++ src/test/java/by/tiranid/utils/TestMainUtils.java
@@ -0,0 +1,22 @@
+package by.tiranid.utils;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestMainUtils {
+
+
+    // config.properties is created
+    @Test
+    public void testLoadDefaultProperties() throws Exception {
+        MainUtils.loadDefaultProperties();
+        String user = MainClientProperties.properties.getProperty("userLogin");
+        Assert.assertEquals(user, "tiranid");
+    }
+
+    @Test
+    public void testSetUpLoggerConfig() {
+        MainUtils.setUpLoggerConfig();
+    }
+}
